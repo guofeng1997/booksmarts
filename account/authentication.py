17f7a71e9f6c70e55522cba728c74a7698e2b7d1
fengguo <1185477572@qq.com>
2019-07-08 15:39:51 +0800

    first commit

---
+++ account/authentication.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+
+
+class EmailAuthBackend:
+    """
+    Authenticate use an Email address.
+    """
+
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            user = User.objects.get(id=user_id)
+            return user
+        except User.DoesNotExist:
+            return None
